ca169917e0b96f4d59ec0a2cddd69a64c691013d
Gustavo Eloi <gustavooeloi@gmail.com>
2024-01-23 14:50:58 -0300

    estudando modulos (export e import)

---
+++ javascript-tooling-es6-modules/aula118/src/index.js
@@ -0,0 +1,6 @@
+import { sum, nome } from "./module1";
+
+const soma = sum(1, 2);
+
+console.log(nome);
+console.log(soma);
---
+++ javascript-tooling-es6-modules/aula118/src/module1.js
@@ -0,0 +1,7 @@
+const sum = (x, y) => {
+  return x + y;
+};
+
+const nome = "gustavo";
+
+export { sum, nome };
