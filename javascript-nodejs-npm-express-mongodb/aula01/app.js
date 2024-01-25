ce14ecfc71b5bc4a89d869124f25f1bab4727739
Gustavo Eloi <gustavooeloi@gmail.com>
2024-01-25 09:38:31 -0300

    entendendo os modulos no node

---
+++ javascript-nodejs-npm-express-mongodb/aula01/app.js
@@ -0,0 +1,9 @@
+// const axios = require("axios");
+
+// axios("https://api.adviceslip.com/advice").then((e) => console.log(e.data));
+
+// fetch("https://api.adviceslip.com/advice")
+//   .then((e) => e.json())
+//   .then((e) => console.log(e));
+
+console.log(__dirname);
---
+++ javascript-nodejs-npm-express-mongodb/aula01/modulo01.js
@@ -0,0 +1,19 @@
+// const nome = "Gustavo";
+// const sobrenome = "Eloi";
+
+// const falaNome = () => {
+//   console.log("Olá, " + nome + " " + sobrenome);
+// };
+
+class Pessoa(nome, anoNascimento){
+  constructor(nome, anoNascimento)
+
+}
+
+module.exports = {
+  // nome,
+  // sobrenome,
+  // falaNome,
+};
+
+
